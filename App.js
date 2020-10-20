82c123754c6f064fbb738d6da29dd5451b33110c
Mateusz Mądel <mateuszm7234@gmail.com>
2020-10-20 16:06:09 +0200

    lab3 z2

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: false,
+        }
+        this.styles = StyleSheet.create({
+                container: {
+                    flex: 1,
+                    backgroundColor: '#fff',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                },
+                button: {
+                    alignItems: "center",
+                    backgroundColor: "#DDDDDD",
+                    padding: 10,
+                    margin:10
+                },
+            });
+    }
+
+    onPress = () => this.setState({visible: !this.state.visible});
+
+    render() {
+        return (
+
+            <View style={this.styles.container}>
+                <Text>Zadanie 2</Text>
+                <TouchableOpacity
+                    style={this.styles.button}
+                    onPress={this.onPress}
+                >
+                    {
+                        this.state.visible ? (<View><Text>Ukryj</Text></View>) : (<View><Text>Pokaz</Text></View>)
+                    }
+
+                </TouchableOpacity>
+                {
+                    this.state.visible ? (<View><Text>Mateusz Mądel</Text></View>) : null
+                }
+                <StatusBar style="auto"/>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
